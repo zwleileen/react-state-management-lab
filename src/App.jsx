f94a4c9a005d438f4087cf2915abe4c0b0c5c26f
eileen <zwleileen@gmail.com>
2025-01-14 23:53:12 +0800

    feat: added features

--- src/App.jsx
+++ src/App.jsx
@@ -1,115 +1,42 @@
 // src/App.jsx
 import './App.css'
 import { useState } from "react";
+import ZombieFighters from './components/ZombieFighters';
 
 const App = () => {
-
-  const [team, setTeam] = useState([])
   const [money, setMoney] = useState(100)
-  const [zombieFighters, setZombieFighters] = useState(
-    [
-      {
-        id: 1,
-        name: 'Survivor',
-        price: 12,
-        strength: 6,
-        agility: 4,
-        img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/0c2d6b.png',
-      },
-      {
-        id: 2,
-        name: 'Scavenger',
-        price: 10,
-        strength: 5,
-        agility: 5,
-        img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/033a16.png',
-      },
-      {
-        id: 3,
-        name: 'Shadow',
-        price: 18,
-        strength: 7,
-        agility: 8,
-        img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/262c36.png',
-      },
-      {
-        id: 4,
-        name: 'Tracker',
-        price: 14,
-        strength: 7,
-        agility: 6,
-        img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/3c1e70.png',
-      },
-      {
-        id: 5,
-        name: 'Sharpshooter',
-        price: 20,
-        strength: 6,
-        agility: 8,
-        img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/4b2900.png',
-      },
-      {
-        id: 6,
-        name: 'Medic',
-        price: 15,
-        strength: 5,
-        agility: 7,
-        img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/5a1e02.png',
-      },
-      {
-        id: 7,
-        name: 'Engineer',
-        price: 16,
-        strength: 6,
-        agility: 5,
-        img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/5e103e.png',
-      },
-      {
-        id: 8,
-        name: 'Brawler',
-        price: 11,
-        strength: 8,
-        agility: 3,
-        img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/67060c.png',
-      },
-      {
-        id: 9,
-        name: 'Infiltrator',
-        price: 17,
-        strength: 5,
-        agility: 9,
-        img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/ac3220.png',
-      },
-      {
-        id: 10,
-        name: 'Leader',
-        price: 22,
-        strength: 7,
-        agility: 6,
-        img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/e41f26.png',
-      },
-    ])
-
-
+  const [team, setTeam] = useState([])
+  
   return (
     <>
-    <h1>Zombie Fighters</h1>
-    <h2>Money: {money}</h2>
-    <h2>Team Strength: </h2>
-    <h2>Team Agility: </h2>
-    <h2>Team</h2>
-    <p>Pick some team members</p>
-    <h2>Fighters</h2>
-    <ul className="character">
-      <img src ='https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/e41f26.png'/>
-      <p><strong>Name</strong></p>
-      <p>Price: </p>
-      <p>Strength: </p>
-      <p>Agility: </p>
-      <button>Add</button>
-    </ul>
+      <h1>Zombie Fighters</h1>
+      <h2>Money: {money}</h2>
+      <h2>Team Strength: </h2>
+      <h2>Team Agility: </h2>
+      <h2>Team</h2>
+      {team.length ===0 ? (
+        <p>Pick some team members</p>
+      ) : (
+        <div className="members">
+          {team.map((member) => (
+            <ul className="member" key={member.id}>
+              <img src ={member.img}/>
+              <p><strong>{member.name}</strong></p>
+              <p>Price: {member.price}</p>
+              <p>Strength: {member.strength}</p>
+              <p>Agility: {member.agility}</p>
+            </ul>
+        ))}
+        </div>
+      )}
+      
+      <h2>Fighters</h2>
+      <ZombieFighters team={team} setTeam={setTeam} money={money} setMoney={setMoney}/> 
     </>
   );
 }
 
 export default App
+
+//Notes: Here, we create the state of team and setTeam, then pass them as props to ZombieFighters i.e. team sends the team array and setTeam sends the function to update the team
+//In ZombieFighters, we receive the props using destructuring i.e. function ZombieFighters({team, setTeam})
